98d7eb49d074bb97f810140c6a9d90f046c88920
Julia Stróżniak <strozniak.julia2507@gmail.com>
2022-01-16 09:51:05 +0100

    [4] The whole process of taking care of the horse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pyautogui
-#Przerwa po kazdym wywolaniu pyautogui - tu 1,5 sekundy
-pyautogui.PAUSE = 1.5
+#Przerwa po kazdym wywolaniu pyautogui - tu 2.5 sekundy
+pyautogui.PAUSE = 2.5
 
 # Zarejestrowanie w osrodku
 registration = pyautogui.locateCenterOnScreen('Image\Registration.jpg', confidence=0.9)
@@ -16,10 +16,34 @@ pyautogui.click()
 box = pyautogui.locateCenterOnScreen('Image\Box.jpg', confidence=0.9, grayscale=False)
 pyautogui.moveTo(box.x, box.y, 0.5, pyautogui.easeOutQuad)
 pyautogui.click()
+
 # Nakarmienie
+
 feeding = pyautogui.locateCenterOnScreen('Image\Feed.jpg', confidence=0.9)
 pyautogui.moveTo(feeding.x, feeding.y, 0.5, pyautogui.easeOutQuad)
 pyautogui.click()
+
+quantity = pyautogui.locateCenterOnScreen('Image\quantity.jpg', confidence=0.95)
+pyautogui.moveTo(quantity.x, quantity.y, 0.5, pyautogui.easeOutQuad)
+pyautogui.click()
+
+feed_it = pyautogui.locateCenterOnScreen('Image\Feeding.jpg', confidence=0.9)
+pyautogui.moveTo(feed_it.x, feed_it.y, 0.5, pyautogui.easeOutQuad)
+pyautogui.click()
+
 # Oporzadzenie
+groom = pyautogui.locateCenterOnScreen('Image\Groom.jpg', confidence=0.9)
+pyautogui.moveTo(groom.x, groom.y, 0.5, pyautogui.easeOutQuad)
+pyautogui.click()
+
 # Polozenie spac
+
+sleep_horse = pyautogui.locateCenterOnScreen('Image\Sleep.jpg', confidence=0.9)
+pyautogui.moveTo(sleep_horse.x, sleep_horse.y, 0.5, pyautogui.easeOutQuad)
+pyautogui.click()
+
 # Przejscie do nastepnego konia
+
+n_horse = pyautogui.locateCenterOnScreen('Image\Arrow.jpg', confidence=0.95)
+pyautogui.moveTo(n_horse.x, n_horse.y, 0.5, pyautogui.easeOutQuad)
+pyautogui.click()
