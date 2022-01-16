98a316f54aa14564a56f2e232b842889d3ddb701
Julia Stróżniak <strozniak.julia2507@gmail.com>
2022-01-16 09:05:08 +0100

    [3] Registration, the beginning of feeding

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 import pyautogui
-#Przerwa po kazdym wywolaniu pyautogui - tu 2,5 sekundy
-pyautogui.PAUSE = 2.5
+#Przerwa po kazdym wywolaniu pyautogui - tu 1,5 sekundy
+pyautogui.PAUSE = 1.5
 
 # Zarejestrowanie w osrodku
-registration = pyautogui.locateCenterOnScreen('Registration.jpg', confidence=0.8)
+registration = pyautogui.locateCenterOnScreen('Image\Registration.jpg', confidence=0.9)
 print(registration)
-pyautogui.moveTo(registration.x, registration.y, 0.9)
+pyautogui.moveTo(registration.x, registration.y, 0.5, pyautogui.easeOutQuad)
+pyautogui.click()
+
+reserved = pyautogui.locateCenterOnScreen('Image\Reserved.jpg', confidence=0.9)
+pyautogui.moveTo(reserved.x, reserved.y, 0.5, pyautogui.easeOutQuad)
+pyautogui.click()
+
+box = pyautogui.locateCenterOnScreen('Image\Box.jpg', confidence=0.9, grayscale=False)
+pyautogui.moveTo(box.x, box.y, 0.5, pyautogui.easeOutQuad)
 pyautogui.click()
 # Nakarmienie
+feeding = pyautogui.locateCenterOnScreen('Image\Feed.jpg', confidence=0.9)
+pyautogui.moveTo(feeding.x, feeding.y, 0.5, pyautogui.easeOutQuad)
+pyautogui.click()
 # Oporzadzenie
 # Polozenie spac
 # Przejscie do nastepnego konia
