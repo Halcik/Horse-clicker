d697bd7d1eaf76fe5488f8e3d1b537b0aae42492
Halcik <julkatablet2507@gmail.com>
2022-07-14 23:47:42 +0200

    corection if instructions and feed variable in function feed

--- feed.py
+++ feed.py
@@ -5,20 +5,20 @@ def h_feed( r, v):
     feeding = pg.locateCenterOnScreen('Image\Feed.jpg', confidence=0.9)
     care = pg.locateCenterOnScreen('Image\Care.jpg', confidence=0.9)
 
-    if (feeding != None):
+    if feeding:
         pg.moveTo(feeding.x, feeding.y, r, pg.easeOutQuad)
         #print(feeding.x, feeding.y)
         pg.click()
 
-    elif (care != None):
+    elif care:
         pg.moveTo(care.x, care.y, r, pg.easeOutQuad)
         pg.click()
     
-    if (v != 1 and care == None and feeding):
+    if v != 1 and care == None and feeding:
         try:
-            Feed0 = pg.locateCenterOnScreen('Image\Feed0.jpg', confidence=0.93)
-            print("0: ", Feed0)
-            if Feed0:
+            feed0 = pg.locateCenterOnScreen('Image\Feed0.jpg', confidence=0.93)
+            print("0: ", feed0)
+            if feed0:
                 quantity = pg.locateCenterOnScreen('Image\quantity0.jpg', confidence=0.9)
                 pg.moveTo(quantity.x, quantity.y, r, pg.easeOutQuad)
             else:
@@ -26,72 +26,72 @@ def h_feed( r, v):
 
         except ErrorFeed:
             try:
-                Feed20 = pg.locateCenterOnScreen('Image\Feed20.jpg', confidence=0.9)
-                print("20: ", Feed20)
-                if Feed20:
+                feed20 = pg.locateCenterOnScreen('Image\Feed20.jpg', confidence=0.9)
+                print("20: ", feed20)
+                if feed20:
                     pg.move(196, 172, r, pg.easeOutQuad)
                 else:
                     raise ErrorFeed()
 
             except ErrorFeed:
                 try:
-                    Feed12 = pg.locateCenterOnScreen('Image\Feed12.jpg', confidence=0.9)
-                    print("12: ", Feed12)
-                    if Feed12:
+                    feed12 = pg.locateCenterOnScreen('Image\Feed12.jpg', confidence=0.9)
+                    print("12: ", feed12)
+                    if feed12:
                         pg.move(107, 88 , r, pg.easeOutQuad)
                     else:
                         raise ErrorFeed()
 
                 except ErrorFeed:
                     try:
-                        Feed10 = pg.locateCenterOnScreen('Image\Feed10.jpg', confidence=0.9)
-                        print("10: ", Feed10)
-                        if Feed10:
+                        feed10 = pg.locateCenterOnScreen('Image\Feed10.jpg', confidence=0.9)
+                        print("10: ", feed10)
+                        if feed10:
                             pg.move(85, 88, r, pg.easeOutQuad)
                         else:
                             raise ErrorFeed()
 
                     except ErrorFeed:
                         try:
-                            Feed8 = pg.locateCenterOnScreen('Image\Feed8.jpg', confidence=0.9)
-                            print("8: ", Feed8)
-                            if Feed8:
+                            feed8 = pg.locateCenterOnScreen('Image\Feed8.jpg', confidence=0.9)
+                            print("8: ", feed8)
+                            if feed8:
                                 pg.move(64, 88, r, pg.easeOutQuad)
                             else:
                                 raise ErrorFeed()
 
                         except ErrorFeed:
                             try:
-                                Feed4 = pg.locateCenterOnScreen('Image\Feed4.jpg', confidence=0.9)
-                                print("4: ", Feed4)
-                                if Feed4:
+                                feed4 = pg.locateCenterOnScreen('Image\Feed4.jpg', confidence=0.9)
+                                print("4: ", feed4)
+                                if feed4:
                                     pg.move(21, 88, r, pg.easeOutQuad)
                                 else:
                                     raise ErrorFeed()
 
                             except ErrorFeed:
                                 try:
-                                    Feed14 = pg.locateCenterOnScreen('Image\Feed14.jpg', confidence=0.9)
-                                    print("14: ", Feed14)
-                                    if Feed14:
+                                    feed14 = pg.locateCenterOnScreen('Image\Feed14.jpg', confidence=0.9)
+                                    print("14: ", feed14)
+                                    if feed14:
                                         pg.move(130, 88, r, pg.easeOutQuad)
                                     else:
                                         raise ErrorFeed()
 
                                 except ErrorFeed:
                                     try:
-                                        Feed6 = pg.locateCenterOnScreen('Image\Feed6.jpg', confidence=0.9)
-                                        print("6: ", Feed6)
-                                        if Feed6:
+                                        feed6 = pg.locateCenterOnScreen('Image\Feed6.jpg', confidence=0.9)
+                                        print("6: ", feed6)
+                                        if feed6:
                                             pg.move(42, 88, r, pg.easeOutQuad)
                                         else:
                                             raise ErrorFeed()
 
                                     except ErrorFeed:
                                         try:
-                                            Feed16 = pg.locateCenterOnScreen('Image\Feed16.jpg', confidence=0.93)
-                                            print("16: ", Feed16)
-                                            if Feed16:
+                                            feed16 = pg.locateCenterOnScreen('Image\Feed16.jpg', confidence=0.93)
+                                            print("16: ", feed16)
+                                            if feed16:
                                                 pg.move(152, 88, r, pg.easeOutQuad)
 
                                         except:
