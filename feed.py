019114a3bf5bebc7c7c700e55f309a35f27035fc
Halcik <julkatablet2507@gmail.com>
2022-07-14 11:26:14 +0200

    adding a feeding alternative without VIP

--- feed.py
+++ feed.py
@@ -16,31 +16,31 @@ def h_feed( r, v):
     
     if (v != 1 and care == None):
         Feed14 = pg.locateCenterOnScreen('Image\Feed14.jpg', confidence=0.9)
-        #print("14: ", Feed14)
+        print("14: ", Feed14)
 
         Feed12 = pg.locateCenterOnScreen('Image\Feed12.jpg', confidence=0.9)
-        #print("12: ", Feed12)
+        print("12: ", Feed12)
 
         Feed10 = pg.locateCenterOnScreen('Image\Feed10.jpg', confidence=0.9)
-        #print("10: ", Feed10)
+        print("10: ", Feed10)
 
         Feed8 = pg.locateCenterOnScreen('Image\Feed8.jpg', confidence=0.9)
-        #print("8: ", Feed8)
+        print("8: ", Feed8)
 
         Feed20 = pg.locateCenterOnScreen('Image\Feed20.jpg', confidence=0.9)
-        #print("20: ", Feed20)
+        print("20: ", Feed20)
 
         Feed6 = pg.locateCenterOnScreen('Image\Feed6.jpg', confidence=0.9)
-        #print("6: ", Feed6)
+        print("6: ", Feed6)
 
         Feed16 = pg.locateCenterOnScreen('Image\Feed16.jpg', confidence=0.93)
-        #print("16: ", Feed16)
+        print("16: ", Feed16)
 
         Feed0 = pg.locateCenterOnScreen('Image\Feed0.jpg', confidence=0.93)
-        #print("0: ", Feed0)
+        print("0: ", Feed0)
 
         Feed4 = pg.locateCenterOnScreen('Image\Feed4.jpg', confidence=0.9)
-        #print("4: ", Feed4)
+        print("4: ", Feed4)
 
         if(Feed0 != None):
             quantity = pg.locateCenterOnScreen('Image\quantity0.jpg', confidence=0.9)
@@ -70,6 +70,9 @@ def h_feed( r, v):
         elif(Feed16 != None):
             pg.move(152, 88, r, pg.easeOutQuad)
 
+        else:
+            pg.move(107, 88 , r, pg.easeOutQuad)
+            print("Zastosowano alternatywę")
         
         pg.click()
 
