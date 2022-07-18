2dca4864bccce88534d8225c68fb95499aa469bd
Halcik <julkatablet2507@gmail.com>
2022-07-18 19:02:59 +0200

    image for next horse on linux

--- next_horse.py
+++ next_horse.py
@@ -6,6 +6,7 @@ def h_next( r):
     n_horse = pg.locateCenterOnScreen('Image/Arrow.jpg', confidence=0.9)
     n_horse2 = pg.locateCenterOnScreen('Image/Arrow2.JPG', confidence=0.9)
     n_horse3 = pg.locateCenterOnScreen('Image/Arrow3.JPG', confidence=0.9)
+    n_horse4 = pg.locateCenterOnScreen('Image/Arrow4.JPG', confidence=0.9)
 
     if n_horse:
         pg.moveTo(n_horse.x, n_horse.y, r, pg.easeOutQuad)
@@ -17,6 +18,10 @@ def h_next( r):
 
     elif n_horse3:
         pg.moveTo(n_horse3.x, n_horse3.y, r, pg.easeOutQuad)
+        pg.click()
+
+    elif n_horse4:
+        pg.moveTo(n_horse4.x, n_horse4.y, r, pg.easeOutQuad)
         pg.click()
 
     else:
