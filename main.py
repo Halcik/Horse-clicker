34fd9274e01553a897fe385d800ab6847b304b39
Halcik <julkatablet2507@gmail.com>
2022-11-11 17:49:54 +0100

    New function with multiplication horses. Adding images for this

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from registration import h_registration
 from groom_functions import h_groom, h_sleep
 from next_horse import h_next
 from death import death
+from multiplication import h_multiplication
 
 import os
 
@@ -16,8 +17,8 @@ pg.PAUSE = 0.3
 n = int(input("Wpisz liczbę koni do oporządzenia:\n"))
 v = int(input("Czy posiadasz vipa?\n 1-Tak\n 2-Nie\n"))
 func_sleep = input("Czy mam kłaść spać? [y/n]\n")
+multiplication = input("Czy chcesz pokryć klacze? [y/n]\n")
 shutdown = input("Czy wyłączyć komputer? [y/n]\n")
-# multiplication = input("Czy chcesz pokryć klacze? [y/n]\n")
 for i in range(n):
     #randomizacja czasu trwania przesunięcia myszki
     random.seed(a=None, version=2)
@@ -31,6 +32,8 @@ for i in range(n):
         if func_sleep == "y" or func_sleep == "Y":
             h_sleep( r)
         death( r)
+        if multiplication == "y" or multiplication == "Y":
+            h_multiplication( r)
     print("Postęp:", i+1, "/", n)
     h_next( r)
 if shutdown=="Y" or shutdown=='y':
--- multiplication.py
+++ multiplication.py
@@ -1,24 +1,24 @@
  # -*- coding: utf-8 -*-
+import pyautogui as pg
+import time
  
- #test rozmnażania
-        # if multiplication == "y" or multiplication == "Y":
-        #     reproduce_check = pg.locateCenterOnScreen('Image/mating_mares.JPG', confidence=0.9)
-        #     if reproduce_check:
-        #         pg.moveTo(reproduce_check.x, reproduce_check.y, r, pg.easeOutQuad)
-        #         pg.click()
-        #         time.sleep(3)
-        #         price_repro = pg.locateCenterOnScreen('Image/price_repro.jpg', confidence=0.8)
-        #         if price_repro:
-        #             pg.moveTo(price_repro.x, price_repro.y, r, pg.easeOutQuad)
-        #             pg.click()
-        #             time.sleep(1)
+def h_multiplication( r):
+    reproduce_check = pg.locateCenterOnScreen('Image/mating_mares.JPG', confidence=0.8)
+    if reproduce_check:
+       pg.moveTo(reproduce_check.x, reproduce_check.y, r, pg.easeOutQuad)
+       pg.click()
+       time.sleep(3)
+       price_repro = pg.locateCenterOnScreen('Image/price_repro.JPG', confidence=0.8)
+       if price_repro:
+              pg.moveTo(price_repro.x, price_repro.y, r, pg.easeOutQuad)
+              pg.click()
+              time.sleep(1)
 
-        #             select_horse = pg.locateCenterOnScreen('Image/select_horse.jpg', confidence=0.8)
-        #             pg.moveTo(select_horse.x, select_horse.y, r, pg.easeOutQuad)
-        #             pg.click()
-        #             time.sleep(2)
+              select_horse = pg.locateCenterOnScreen('Image/select_horse.JPG', confidence=0.8)
+              pg.moveTo(select_horse.x, select_horse.y, r, pg.easeOutQuad)
+              pg.click()
+              time.sleep(2)
 
-        #             mating = pg.locateCenterOnScreen('Image/mating.jpg', confidence=0.8)
-        #             pg.moveTo(mating.x, mating.y, r, pg.easeOutQuad)
-        #             pg.click()
-        #koniec testu
+              mating = pg.locateCenterOnScreen('Image/mating.JPG', confidence=0.8)
+              pg.moveTo(mating.x, mating.y, r, pg.easeOutQuad)
+              pg.click()
